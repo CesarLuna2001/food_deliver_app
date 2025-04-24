d1806adff6ac040b7616a7a8574a1ee08dd8b821
Cesarluna200115 <cesarluna200115@outlook.com>
2025-04-24 17:30:55 -0600

    Payment Page

--- lib/components/my_cart_tile.dart
+++ lib/components/my_cart_tile.dart
@@ -1,10 +1,117 @@
 import 'package:flutter/material.dart';
+import 'package:food_deliver_app/components/my_quantity_selector.dart';
+import 'package:food_deliver_app/models/cart_item.dart';
+import 'package:food_deliver_app/models/restaurant.dart';
+import 'package:provider/provider.dart';
 
 class MyCartTile extends StatelessWidget {
-  const MyCartTile({super.key});
+  final CartItem cartItem;
+
+  const MyCartTile({super.key, required this.cartItem});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Consumer<Restaurant>(
+      builder: (context, restaurant, child) => Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.secondary,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        margin: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //Food image
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Image.asset(
+                      cartItem.food.imagePath,
+                      height: 100,
+                      width: 100,
+                    ),
+                  ),
+              
+                  const SizedBox(width: 10),
+              
+                  //Name and Price
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //Food name
+                      Text(cartItem.food.name),
+              
+                      //Food Price
+                      Text('${cartItem.food.price}', 
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                      ),
+                    ]
+                  ),
+
+                  const Spacer(),
+              
+                  //Increment o Decrement quantity
+                  QuantitySelector(
+                    quantity: cartItem.quantity, 
+                    food: cartItem.food, 
+                    onDecrement: () {
+                      //Decrement quantity
+                      restaurant.removeFromCart(cartItem);
+                    }, 
+                    onIncrement:(){
+                      //Increment quantity
+                      restaurant.addToCart(cartItem.food, cartItem.selectedAddons);
+                    }
+              
+                  )
+                ],
+              ),
+            ),
+
+            //Addons
+            SizedBox(
+              height: cartItem.selectedAddons.isEmpty? 0: 60,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                padding: const EdgeInsets.only(left: 10, bottom: 10, right: 10),
+                children: cartItem.selectedAddons.map(
+                  (addon) => Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: FilterChip(
+                      label: Row(
+                        children: [
+                          //Addon Name
+                          Text(addon.name),
+                    
+                          //Adon Price
+                          Text('${addon.price}'),
+                        ],
+                      ), 
+                      shape: StadiumBorder(
+                        side: BorderSide(
+                          color: Theme.of(context).colorScheme.primary,
+                        )
+                      ),
+                      onSelected: (value) {},
+                      backgroundColor: Theme.of(context).colorScheme.secondary,
+                      labelStyle: TextStyle(
+                        color: Theme.of(context).colorScheme.inversePrimary,
+                        fontSize: 12,
+                      ),
+                    ),
+                  )
+                ).toList(),
+              )
+            )
+
+          ],
+        ),
+      )
+    );
   }
 }
--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:food_deliver_app/components/my_button.dart';
+import 'package:food_deliver_app/components/my_cart_tile.dart';
 import 'package:food_deliver_app/models/restaurant.dart';
+import 'package:food_deliver_app/pages/payment_page.dart';
 import 'package:provider/provider.dart';
 
 class CartPage extends StatelessWidget {
@@ -15,27 +18,83 @@ class CartPage extends StatelessWidget {
         //Scaffold UI
         return Scaffold(
           appBar: AppBar(
-            title: Text("Cart"),
+            title: const Text("Cart"),
             backgroundColor: Colors.transparent,
             foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+            actions: [
+              //Clear cart button
+              IconButton(
+                onPressed: () {
+                  showDialog(
+                    context: context, 
+                    builder: (context) => AlertDialog(
+                      title: const Text("Are you sure you want to clear the cart?"),
+                      actions: [
+                        //Cancel Button
+                        TextButton(
+                          onPressed: () => Navigator.pop(context),
+                          child: const Text("Cancel")
+                        ),
+
+                        //Yes Button
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                            restaurant.clearCart();
+                          },
+                          child: const Text("Yes")
+                        )
+                      ],
+                    )
+                  );
+                },
+                icon: Icon(Icons.delete),
+              )
+            ],
           ),
           body: Column(
             children: [
+
+              //List of cart 
               Expanded(
-                child: ListView.builder(
-                  itemCount: userCart.length,
-                  itemBuilder: (context, index) {
-                    //get individual cart item 
-                    final cartItem = userCart[index];
-                    //return cart tile UI
-                    return ListTile(
-                      title: Text(cartItem.food.name),
-                    );
-                  }
-                )
-              )
+                child: Column(
+                  children: [
+                    userCart.isEmpty 
+                    ? const Expanded(
+                      child: Center(
+                        child: Text("Cart is empty...")
+                      )
+                    ) 
+                    : Expanded(
+                      child: ListView.builder(
+                        itemCount: userCart.length,
+                        itemBuilder: (context, index) {
+                          //get individual cart item 
+                          final cartItem = userCart[index];
+                          //return cart tile UI
+                          return MyCartTile(cartItem: cartItem);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              //My button to pay 
+              MyButton(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                  builder: (context) => const PaymentPage()
+                  )
+                ), 
+                text: "Go to checkout",
+              ),
+
+              const SizedBox(height: 25),
+
             ],
-          ) 
+          ), 
         );
       }
     );
